645c1278991e06dc764e4f93b06587b1e06bc8b8
Zhang Yuan <zhangyuan@ict.ac.cn>
2016-08-22 20:05:00 +0800

    Update 0822
    Add Q3_3, Q3_4

---
+++ Q3_3.cpp
@@ -0,0 +1,197 @@
+//Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+//Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. 
+//Implement a data structure SetOfStacks that mimics this.    
+//SetOfStacks should be composed of several stacks, and should create a new stack once the previous one exceeds capacity.  
+//SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack (that is, pop() should return the same valuse as it would if there were just a single stack). 
+//FOLLOW UP
+//Implement a functiopm popAt(int index) which performs a pop operation on a specific sub-stack
+
+#include <iostream>
+
+using namespace std;
+
+const int STACK_SIZE = 10;
+const int STACK_NUM = 10;
+
+class stack
+{
+    private:
+        int *buf;
+        int cur;
+        int capacity;
+    public:
+        stack(int capa = STACK_SIZE)
+        {
+            buf = new int[capa];
+            cur = -1;
+            capacity = capa;
+        }
+        ~stack()
+        {
+            delete[] buf;
+        }
+        void push(int val)
+        {
+            buf[++cur] = val;
+        }
+        void pop()
+        {
+            --cur;
+        }
+        int top()
+        {
+            return buf[cur];
+        }
+        bool empty()
+        {
+            return cur == -1;
+        }
+        bool full()
+        {
+            return cur == capacity - 1;
+        }
+        void print()
+        {
+            for(int i = 0; i < cur; i++)
+            {
+                cout<<buf[i]<<endl;
+            }
+        }
+};
+
+//Without popAt()
+class SetOfStacks
+{
+    private:
+        stack *st;
+        int cur;
+        int capacity;
+    public:
+        SetOfStacks(int capa = STACK_NUM)
+        {
+            st = new stack[capa];
+            cur = 0;
+            capacity = capa;
+        }
+        ~SetOfStacks()
+        {
+            delete[] st;
+        }
+        void push(int val)
+        {
+            if(st[cur].full()) ++cur;
+            st[cur].push(val);
+        }
+        void pop()
+        {
+            if(st[cur].empty()) --cur;
+            st[cur].pop();
+        }
+        int top()
+        {
+            if(st[cur].empty()) --cur;
+            return st[cur].top();
+        }
+        bool empty()
+        {
+            if(cur == 0) return st[0].empty();
+            else return false;
+        }
+        bool full()
+        {
+            if(cur == capacity - 1) return st[cur].full();
+            else return false;
+        }
+};
+
+class SetOfStacks_1
+{
+    private:
+        stack *st;
+        int cur;
+        int capacity;
+    public:
+        SetOfStacks_1(int capa = STACK_NUM)
+        {
+            st = new stack[capa];
+            cur = 0;
+            capacity = capa;            
+        }
+        ~SetOfStacks_1()
+        {
+            delete[] st;
+        }
+        void push(int value)
+        {
+            if(st[cur].full()) cur++;
+            st[cur].push(value);
+        }
+        void pop()
+        {
+            while(st[cur].empty()) cur--;
+            st[cur].pop();
+        }
+        void popAt(int index)
+        {
+            if(st[index].empty())
+            {
+                cout<<"Index: "<<index<<" empty!"<<endl;
+            }
+            else
+            {
+                st[index].pop();
+            }
+        }
+        bool empty()
+        {
+            while(cur != -1 && st[cur].empty()) --cur;
+            return (cur == -1);
+        }
+        bool full()
+        {
+            bool flag = true;
+            for(int i = 0; i < capacity; i++)
+            {
+                if(!st[i].full()) flag = false;
+            }
+            return flag;
+        }
+        int top()
+        {
+            while(st[cur].empty()) cur--;
+            return st[cur].top();
+        }
+        void print(int index)
+        {
+            cout<<"Print Index: "<<index<<endl;
+            st[index].print();
+        }
+        
+};
+
+int main()
+{
+    SetOfStacks sos;
+    for(int i = 0; i < 99; i++)
+    {
+        sos.push(i);
+        //cout<<"Stack top: "<<sos.top()<<endl;
+    }
+    cout<<"Check full: "<<sos.full()<<endl;
+
+    SetOfStacks_1 sos_1;
+    for(int i = 0; i < 100; i++)
+    {
+        sos_1.push(i);
+        //cout<<"Stack top: "<<sos_1.top()<<endl;
+    }
+    cout<<"Check full: "<<sos_1.full()<<endl;
+    sos_1.print(5);
+    sos_1.popAt(5);
+    sos_1.print(5);
+    sos_1.popAt(5);
+    sos_1.print(5);
+    cout<<"Check full: "<<sos_1.full()<<endl;
+  
+    return 0;
+}
---
+++ Q3_4.cpp
@@ -0,0 +1,148 @@
+//In the classic problem of the Towers of Hanoi, you have 3 rods and N disks of different sizes which can slide onto any tower.
+//The puzzle starts with disks sorted in ascending order of size from top to bottom (e.g. each disk sits on top of an even larger one).  
+//You have the following constraints:
+//Only one disk can be moved at a time.
+//A disk is slid off the top of one rod onto the next rod.
+//A disk can only be placed on top of a larger disk.   
+//Write a program to move the disks from the first rod to the last using Stacks.
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct op
+{
+    int begin, end;
+    char src, bri, dst;
+    op(){}
+    op(int pbegin, int pend, int psrc, int pbri, int pdst):begin(pbegin), end(pend), src(psrc), bri(pbri), dst(pdst)
+    {
+    }
+};
+
+void hanoi(int n, char src, char bri, char dst)
+{
+    stack<op> st;
+    op tmp;
+    st.push(op(1, n, src, bri, dst));
+    while(!st.empty())
+    {
+        tmp = st.top();
+        st.pop();
+        if(tmp.begin != tmp.end)
+        {
+            st.push(op(tmp.begin, tmp.end - 1, tmp.bri, tmp.src, tmp.dst));
+            st.push(op(tmp.end, tmp.end, tmp.src, tmp.bri, tmp.dst));
+            st.push(op(tmp.begin, tmp.end - 1, tmp.src, tmp.dst, tmp.bri));
+        }
+        else
+        {
+            cout<<"Move Disk "<<tmp.begin<<" from "<<tmp.src<<" to "<<tmp.dst<<endl;
+        }
+    }
+}
+
+
+class Stacks
+{
+    public:
+        Stacks(int size = 20)
+        {
+            this->size = size;
+            buf = new int[size];
+            cur = -1;
+        }
+
+        ~Stacks()
+        {
+            delete []buf;
+        }
+
+        void push(int value)
+        {
+            cur++;
+            buf[cur] = value;
+        }
+
+        int pop()
+        {
+            int value = buf[cur];
+            cur--;
+            return value;
+        }
+
+        int top()
+        {
+            return buf[cur];
+        }
+
+        bool empty()
+        {
+            return (cur == -1);
+        }
+
+        void print()
+        {
+            for(int i = 0; i <= cur; i++)
+            {
+                cout<<buf[i]<<" ";
+            }
+            cout<<endl;
+        }
+
+    private:
+        int cur;
+        int *buf;
+        int size;
+};
+
+void Hanoi_Tower(int start_plate, int end_plate, int first_stack, int end_stack, int middle_stack)
+{
+    if(start_plate == end_plate)    cout<<"Moving: "<<start_plate<<" from "<<first_stack<<" to "<<end_stack<<" through "<<middle_stack<<endl;
+    else
+    {
+          Hanoi_Tower(start_plate, end_plate - 1, first_stack, middle_stack, end_stack);
+          Hanoi_Tower(end_plate, end_plate, first_stack, end_stack, middle_stack);
+          Hanoi_Tower(start_plate, end_plate - 1, middle_stack, end_stack, first_stack);
+    }
+}
+
+void Hanoi_Tower_1(int num, Stacks* first_stack, Stacks* end_stack, Stacks* middle_stack)
+{
+
+    if(num == 1)
+    {
+        int exchange = first_stack->pop();
+        cout<<"Exchange: "<<exchange<<endl;
+        end_stack->push(exchange);
+    }    
+    else
+    {
+        Hanoi_Tower_1(num - 1, first_stack, middle_stack, end_stack);
+        Hanoi_Tower_1(1, first_stack, end_stack, middle_stack);
+        Hanoi_Tower_1(num - 1, middle_stack, end_stack, first_stack);
+    }
+}
+
+int main()
+{
+    //Hanoi_Tower(1, 3, 1, 3, 2);
+    Stacks first_stack, middle_stack, end_stack;
+    int range = 4;
+    for(int i = range; i > 0; i--)
+    {
+        first_stack.push(i);
+    }
+    cout<<"Initial:"<<endl;
+    first_stack.print();
+    Hanoi_Tower_1(range, &first_stack, &middle_stack, &end_stack);
+    cout<<"First Stack: "<<endl;
+    first_stack.print();
+    cout<<"Middle Stack: "<<endl;
+    middle_stack.print();
+    cout<<"End Stack: "<<endl;
+    end_stack.print();
+
+    hanoi(3, 'A', 'B', 'C');
+    return 0;
+}
