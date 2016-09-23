31e19a8efb333f6cd651ee46a7065e34551409a8
Zhang Yuan <zhangyuan@ict.ac.cn>
2016-09-23 19:57:18 +0800

    Update 0923
    Add Q8_5.

---
+++ Q8_5.cpp
@@ -0,0 +1,44 @@
+//Implement an algorithm to print all valid (e.g., properly opened and closed) combinations of n-pairs of parenthese. 
+//EXAMPLE:
+//input: 3 (e.g., 3 pairs of parentheses)
+//output: ((())), (()()), (())(), ()(()), ()()()
+#include <iostream>
+
+using namespace std;
+
+void parentheses(char *a, int n, int match, int pos)
+{
+    if(match == 0 && n == 0)
+    {
+        a[pos] = '\0';
+        cout<<"Res: "<<a<<endl;
+        return;
+    }
+    if(n == 0 && match != 0)
+    {
+        a[pos] = ')';
+        parentheses(a, n, match - 1, pos + 1);
+        return;
+    }
+    if(n != 0 && match == 0)
+    {
+        a[pos] = '(';
+        parentheses(a, n - 1, match + 1, pos + 1);
+    }
+    else
+    {
+        a[pos] = ')';
+        parentheses(a, n, match - 1, pos + 1);
+        a[pos] = '(';
+        parentheses(a, n - 1, match + 1, pos + 1);
+    }
+    return;
+}
+
+
+int main()
+{
+    char a[100];
+    parentheses(a, 3, 0, 0);
+    return 0;
+}
