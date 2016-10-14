d7cd8f88215e43fa48ebf9e4f33501826a2910be
Zhang Yuan <zhangyuan@ict.ac.cn>
2016-10-14 21:01:33 +0800

    Update 1014
    Add GCD and prime.

---
+++ GCD.cpp
@@ -0,0 +1,31 @@
+//最大公约数
+
+#include <iostream>
+
+using namespace std;
+
+//辗转相除法 a = m * b + c 如果存在d是b和c的最大公约数，那么也就a和b的最大公约数
+int GCD(int a, int b)
+{
+    if( a < b)
+    {
+        int d = a;
+        a = b;
+        b = d;
+    }
+    if(a % b == 0)
+    {
+        return b;
+    }
+    else
+    {
+        int c = a % b;
+        return GCD(b, c);
+    }
+}
+
+int main()
+{
+    cout<<GCD(6, 23)<<endl;
+    return 0;
+}
---
+++ prime.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+//一般来讲，判断一个数n是不是质数，就是判断从1到n-1，能否除了1，还存在其他数可以整除它
+//n = a * b = \sqrt{n} * \sqrt{n}
+//所以判断从1到sqrt(n)中是否存在整除数就足够了，此时的时间复杂度是O(logn)
+//但是如果n非常大时，\sqrt(n)也很大，这样也很慢。
+//开一个bool数组，然后从2开始，到sqrt(n)结束，2是质数，那么2的倍数全是合数，全标记好
+//3是质数，然后3的倍数全是合数，以此类推，最后看bool数组中谁没被标记，谁就是质数，这样可以避免很多重复操作
+void prime(int n)
+{
+    if(n == 1)
+    {
+        cout<<"Not prime"<<endl;
+        return;
+    }
+    bool flag = false;
+    int sqrt_n = sqrt(n);
+    for(int i = 2; i <= sqrt_n; i++)
+    {
+        if(n % i == 0)
+        {
+            flag = true;
+            break;
+        }
+    } 
+    if(flag == true)
+    {
+        cout<<"Not prime"<<endl;
+    }
+    else
+    {
+        cout<<"Prime"<<endl;
+    }
+}
+
+int main()
+{
+    int n = 6;
+    prime(n);
+    return  0;
+}
