cd59839296ed3cc2877b371417f4db5b596c65cc
Zhang Yuan <zhangyuan@ict.ac.cn>
2017-01-28 22:40:42 +0800

    Update 0128
    Update Q5.

--- Leetcode/Q5.cpp
+++ Leetcode/Q5.cpp
@@ -11,6 +11,7 @@ Input: "cbbd"
 
 Output: "bb"
 */
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -18,15 +19,69 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int *arr = new int[s.length()];
+        //这样方法声明的二维数组是间断的
+        /*
+        连续二维数组声明
+        int *array = new int[s.length() * s.length()];
+        int **arr;
+        arr = new int *[s.length()];
+        for(int i = 0; i < s.length())
+            arr[i] = array + i * s.elngth();
+        */
+        int flag = -1;
+        int max = 0;
+
+        int **arr = new int*[s.length()];
         for(int i = 0; i < s.length(); ++i)
         {
-            if(i = 0) arr[i] = 1;
-            else
+            //数组初始化就是在后面加个()
+            arr[i] = new int[s.length()]();
+        }
+
+        for(int i = 0; i < s.length(); ++i)
+        {
+            for(int j = 0; j <= i; ++j)
             {
-                
+                if(j == 0) arr[i][j] = 1;
+                else if(j == 1)
+                {
+                    if(s[i] == s[i-1]) arr[i][j] = 2;
+                    else arr[i][j] = 0;
+                }
+                else if(j == 2)
+                {
+                    if(s[i] == s[i-2]) arr[i][j] = 3;
+                    else arr[i][j] = 0;
+                }
+                else
+                {
+                    if(arr[i-1][j-2] != 0 && s[i] == s[i-arr[i-1][j-2] - 1]) arr[i][j] = j + 1;
+                    else arr[i][j] = 0;
+                }
+
+                if(arr[i][j] >= max)
+                {
+                    flag = i;
+                    max = arr[i][j];
+                }
             }
         }
+
+        /*for(int i = 0; i < s.length(); ++i)
+        {
+            for(int j = 0; j < s.length(); ++j)
+            {
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }*/
+
+        string ans = "";
+        for(int i = 0; i < max; ++i)
+        {
+            ans = s[flag - i] + ans;
+        }
+        return ans;
     }
 };
 
@@ -42,5 +97,8 @@ int main()
     ans = s1.longestPalindrome(s);
     cout<<"Ans is: "<<ans<<endl;
 
+    s = "bbabba";
+    ans = s1.longestPalindrome(s);
+    cout<<"Ans is: "<<ans<<endl;
     return 0;
 }
