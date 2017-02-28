3c8187bef24d1db5762e5d649d01b713d927a733
Zhang Yuan <zhangyuan@ict.ac.cn>
2017-02-28 22:08:17 +0800

    Update 0228
    Add Q30.

--- Leetcode/Q30.cpp
+++ Leetcode/Q30.cpp
@@ -16,10 +16,69 @@ You should return the indices: [0,9].
 #include <vector>
 #include <string>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution
+{
+    public:
+        vector<int> findSubstring(string s, vector<string>& words)
+        {
+            vector<int> res;
+            int n = s.size();
+            int cnt = words.size();
+            if(n <= 0 || cnt <= 0) return res;
+
+            unordered_map<string, int> dict; 
+
+            for(int i = 0; i < cnt; ++i) dict[words[i]]++;
+
+            int wl = words[0].size();
+
+            for(int i = 0; i < wl; ++i)
+            {
+                int left = i;
+                int count  = 0;
+                unordered_map<string, int> tdict;
+                for(int j = i; j <= n - wl; j += wl)
+                {
+                    string str = s.substr(j, wl);
+                    if(dict.count(str))
+                    {
+                        tdict[str]++;
+                        if(tdict[str] <= dict[str]) count++;
+                        else
+                        {
+                            while(tdict[str] > dict[str])
+                            {
+                                string str1 = s.substr(left, wl);
+                                tdict[str1]--;
+                                if(tdict[str1] < dict[str1]) count--;
+                                left += wl;
+                            }
+                        }
+                        if(count == cnt)
+                        {
+                            res.push_back(left);
+                            tdict[s.substr(left, wl)]--;
+                            count--;
+                            left += wl;
+                        }
+                    }
+                    else
+                    {
+                        tdict.clear();
+                        count = 0;
+                        left = j + wl;
+                    }
+                }
+            }
+            return res;
+        }
+};
+
+class Solution2
 {
     public:
         vector<int> findSubstring(string s, vector<string>& words)
@@ -53,7 +112,7 @@ class Solution
             }
 
             priority_queue<int, vector<int>, greater<int>> pq1;
-                
+                            
             for(int i = 0; i < words.size(); ++i)
             {
                 if(sup_map[words[i]].size() == 0)
@@ -90,6 +149,7 @@ class Solution
         }
 
     private:
+
             bool make_sure(priority_queue<int, vector<int>, greater<int>> pq, int len_word)
             {
                 int length = pq.size();
@@ -218,8 +278,8 @@ int main()
     string str = "barfoobarthefoobarbarfooman";
     vector<string> vec = {"bar", "foo", "bar"};
 
-    str = "aaaaaa";
-    vec = {"aaa", "aaa"};
+    //str = "aaaaaaa";
+    //vec = {"aaa", "aaa"};
 
     vector<int> res = s.findSubstring(str, vec);
     
