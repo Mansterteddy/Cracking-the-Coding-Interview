08a9990cc8cb3e1ef412cff729c486e817093164
Zhang Yuan <zhangyuan@ict.ac.cn>
2018-07-30 16:55:45 +0800

    Update 0730, add two problems.

--- Interview/interesting problem/dictsort.cpp
+++ Interview/interesting problem/dictsort.cpp
@@ -1,11 +1,76 @@
-//
-#include <iostream>
+/*
+有一个从1到n的数列，按照字典序排列，第k个数字是多少？
+首先处理n，确定它有几位，每一位的数字是多少，使用一个数组将其保存起来。
+随后从1开始处理，1、10、11、...、100、101、...、2、20、21、...、200...
+对应的数据范围为1，1+10，1+10+100，...
+注意对n最高位的特殊处理，判断k是否在此范围内即可。
+举例：
+n=16 k=7
+1 10 11 12 13 14 15 16 2 3 4 5 6 7 8 9
+res = 15
+*/
 
+#include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
 
 int main()
 {
+    cout << "Input n:" << endl;
+    int n = 0;
+    cin >> n;
 
+    cout << "Input k:" << endl;
+    int k = 0;
+    cin >> k;
     
+    if(k > n)
+    {
+        cout << "Null!" << endl;
+        return 0;
+    }
+
+    int m = n;
+    vector<int> range;
+    
+    while(m!=0)
+    {
+        if(m >= 10) range.push_back(m%10);
+        else range.push_back(m);
+        m = m / 10;
+    }
+
+    int res = 0;
+    int index = 0;
+    int pre_index = index;
+
+    for(int i = 1; i <= 9; ++i)
+    {
+        for(int j = 0; j < range.size(); ++j)
+        {
+            pre_index = index;
+            if(j==range.size()-1 && i > range[j])
+            {
+                break;
+            }
+            if(j==range.size()-1 && i==range[j])
+            {
+                index += n - pow(10,j) + 1;
+            }
+            else
+            {
+                index += pow(10, j);
+            }
+
+            if(index >= k)
+            {
+                res = (k - pre_index - 1) + i * pow(10, j);
+                cout << res << endl;
+                return res;
+            }
+        }
+    }
+
     return 0;
 }
--- Interview/interesting problem/loop.cpp
+++ Interview/interesting problem/loop.cpp
@@ -1,6 +1,23 @@
-//有一个环，有n个点，请问从0点出发，经过k步回到原点有多少种方法？
+/*
+有一个环，有n个点，请问从0点出发，经过k步回到原点有多少种方法？
+动态规划问题，第i个点，经过k步回到原点的方法数量为d(i,k)，
+递推公式为：d(i,k) = d(i-1,k-1) + d(i+1,k-1)
+因为是环，因此公式可以改写为：d(i,k) = d((i-1+n)%n,k-1) + d((i+1+n)%n,k-1)
+初始状态：d(0,0)=1, d(1,0)=0, ...
+由于k是递增的，d(0,1)=d(n-1,0)+d(1,0)=0, d(1,1)=d(0,0)+d(2,0)=1
+求取d(0,k)即可。
+由于递归式只包含两列数据，因此在空间限制的情况下，可以只维持两个数组。
+例子：
+假设n=3 k=3
+k\n 0   1   2
+0   1   0   0
+1   0   1   1
+2   2   1   1
+3   2   3   3
+*/
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,7 +31,45 @@ int main()
     int k = 0;
     cin >> k;
 
-    
+    //Special cases:
+    if(n==1) 
+    {
+        cout << 1;
+        return 1;
+    }
+    if(n==2)
+    {
+        if(k%2==0)
+        {
+            cout << 1;
+            return 1;
+        }
+        else 
+        {
+            cout << 0;
+            return 0;
+        }
+    }
 
-    return 0;
+    vector<int> first(n);
+    vector<int> second(n);
+
+    first[0] = 1;
+    for(int i = 1; i < n; ++i)
+    {
+        first[i] = 0;
+    }
+
+    for(int j = 1; j <= k; ++j)
+    {
+        for(int h = 0; h < n; ++h)
+        {
+            second[h] = first[(h-1+n)%n] + first[(h+1+n)%n]; 
+        }
+        first = second;
+    }
+
+    cout<<second[0]<<endl;
+
+    return second[0];
 }
