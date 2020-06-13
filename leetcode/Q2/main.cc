ad8d46bcc0a2b073f5cf2662e788c71af06a7893
Yuan Zhang <mansterteddy@gmail.com>
2020-06-13 15:54:10 +0800

    Add Leetcode Q1 and Q2.

--- leetcode/Q2/main.cc
+++ leetcode/Q2/main.cc
@@ -2,21 +2,19 @@
 
 using namespace std;
 
-// Definition for singly-linked list
-
+// Definition for singly-linked list.
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x): val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
 };
-
+ 
 class Solution{
     public:
         ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
             ListNode* res_start = new ListNode(0);
             ListNode* l1_cur = l1;
             ListNode* l2_cur = l2;
-
             int next = 0;
             ListNode* cur_res = res_start;
             while(l1_cur != NULL || l2_cur != NULL){
@@ -38,36 +36,39 @@ class Solution{
         }
 };
 
-void printLinkNode(ListNode* a){
-    while(a != NULL){
-        cout << a->val << endl;
-        a = a->next;
+void printListNode(ListNode* ptr){
+    while(ptr != NULL){
+        cout << ptr->val << endl;
+        ptr = ptr->next;
     }
 }
 
 int main(){
-    cout << "LeetCode 002" << endl;
 
-    ListNode a_1 = ListNode(2);
-    ListNode a_2 = ListNode(4);
-    ListNode a_3 = ListNode(3);
-    a_1.next = &a_2;
-    a_2.next = &a_3;
+    ListNode a1 = ListNode(2);
+    ListNode a2 = ListNode(4);
+    ListNode a3 = ListNode(3);
+    a1.next = &a2;
+    a2.next = &a3;
 
-    printLinkNode(&a_1);
+    ListNode b1 = ListNode(5);
+    ListNode b2 = ListNode(6);
+    ListNode b3 = ListNode(4);
+    b1.next = &b2;
+    b2.next = &b3;
 
-    ListNode b_1 = ListNode(5);
-    ListNode b_2 = ListNode(6);
-    ListNode b_3 = ListNode(4);
-    b_1.next = &b_2;
-    b_2.next = &b_3;
+    /*
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807 
+    */
 
-    printLinkNode(&b_1);
+    printListNode(&a1);
+    printListNode(&b1);
 
-    Solution so = Solution();
-
-    ListNode* res = so.addTwoNumbers(&a_1, &b_1);
-    printLinkNode(res);
+    Solution s = Solution();
+    auto res = s.addTwoNumbers(&a1, &b1);
+    printListNode(res);
 
     return 0;
 }
