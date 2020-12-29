0ea48d375af7006ba4cb6b03dbeb919f14b07615
mansterteddy <mansterteddy@gmail.com>
2020-12-29 14:12:08 +0800

    Update openmp.

--- HPC/openmp/openmp_1/openmp_1/openmp_1.cpp
+++ HPC/openmp/openmp_1/openmp_1/openmp_1.cpp
@@ -22,5 +22,10 @@ int main()
 
     std::cout << "Finished!" << std::endl;
 
+    #pragma omp parallel for
+    for (int i = 0; i < 10; ++i) {
+        std::cout << i << std::endl;
+    }
+
     return 0;
 }
