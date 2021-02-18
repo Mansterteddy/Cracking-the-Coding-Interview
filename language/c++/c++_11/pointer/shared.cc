1e5566d056e532d1ca6bbd092a5e62d8eee91145
mansterteddy <mansterteddy@gmail.com>
2021-02-18 23:17:10 +0800

    Add shared_ptr.

---
+++ language/c++/c++_11/pointer/shared.cc
@@ -0,0 +1,27 @@
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+void foo(std::shared_ptr<int> i){
+    (*i)++;
+}
+
+int main(){
+
+    auto pointer = make_shared<int>(10);
+    foo(pointer);
+    cout << *pointer << endl;
+
+    auto pointer_1 = make_shared<int>(10);
+    auto pointer_2 = pointer_1;
+    auto pointer_3 = pointer_1;
+
+    int* p = pointer_1.get();
+
+    cout << "pointer_1 use count: " << pointer_1.use_count() << endl;
+    cout << "pointer_2 use count: " << pointer_2.use_count() << endl;
+    cout << "pointer_3 use count: " << pointer_3.use_count() << endl;
+
+    return 0;
+}
