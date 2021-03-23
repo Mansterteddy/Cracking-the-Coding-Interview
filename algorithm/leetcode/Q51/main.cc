bab39ddbca9f2e1d41180f3b1ad8ac83fe2ebe28
mansterteddy <mansterteddy@gmail.com>
2021-03-23 22:42:48 +0800

    Add Q51.

---
+++ algorithm/leetcode/Q51/main.cc
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+    public:
+
+        bool recursiveSolver(){
+
+        }
+
+
+        vector<vector<string>> solveNQueens(int n){
+            for(int i = 0; i < n; ++i){
+                
+            }
+
+
+
+        }
+};
+
+int main(){
+
+    Solution s = Solution();
+    auto res = s.solveNQueens(1);
+
+    return 0;
+}
