f6572628a40fec6e7c9e4c90da1e1ad281fe27e2
mansterteddy <mansterteddy@gmail.com>
2021-03-26 22:39:07 +0800

    Add N Queens.

--- algorithm/leetcode/Q52/main.cc
+++ algorithm/leetcode/Q52/main.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 class Solution{    
     public:
-        void recursiveSolver(vector<int> cur_vec, int cur_layer, int n, vector<vector<string>>& res, vector<string>& template_vec){
+        void recursiveSolver(vector<int> cur_vec, int cur_layer, int n, int& res){
 
             if(cur_layer == n - 1){
                 int cur_index = -1;
@@ -27,16 +27,13 @@ class Solution{
 
                 if(flag){
                     cur_vec.push_back(cur_index);
-                    vector<string> cur_res = template_vec;
-                    for(int i = 0; i < n; ++i){
-                        cur_res[i][cur_vec[i]] = 'Q';
-                    }
-                    res.push_back(cur_res);
+                    res += 1;
                     cur_vec.pop_back();
                 }                
                 return;
             }
-            else{
+            else
+            {
                 for(int i = 0; i < n; ++i){
                     
                     if(find(cur_vec.begin(), cur_vec.end(), i) == cur_vec.end()){
@@ -52,37 +49,25 @@ class Solution{
                         
                         if(flag){
                             cur_vec.push_back(cur_index);
-                            recursiveSolver(cur_vec, cur_layer+1, n, res, template_vec);
+                            recursiveSolver(cur_vec, cur_layer+1, n, res);
                             cur_vec.pop_back();
-                        
                         }
                     }
                 }
             }
         }
 
-        vector<vector<string>> solveNQueens(int n){
+        int totalNQueens(int n){
 
             if(n==1){
-                vector<string> res_item{"Q"};
-                vector<vector<string>> res{res_item};
-                return res;
+                return 1;
             }
 
-            vector<vector<string>> res;
-            vector<string> template_vec;
-            string temp_vec_item = "";
-            for(int i = 0; i < n; ++i){
-                temp_vec_item += ".";
-            }
-            for(int i = 0; i < n; ++i){
-                template_vec.push_back(temp_vec_item);
-            }
-
+            int res = 0;
             for(int i = 0; i < n; ++i){
                 vector<int> cur_vec;
                 cur_vec.push_back(i);
-                recursiveSolver(cur_vec, 1, n, res, template_vec);
+                recursiveSolver(cur_vec, 1, n, res);
                 cur_vec.pop_back();
             }
 
@@ -93,14 +78,9 @@ class Solution{
 int main(){
 
     Solution s = Solution();
-    vector<vector<string>> res = s.solveNQueens(4);
 
-    for(auto item: res){
-        for(auto item_str: item){
-            cout << item_str << endl;
-        }
-        cout << endl;
-    }
+    int res = s.totalNQueens(8);
+    cout << res << endl;
 
     return 0;
 }
