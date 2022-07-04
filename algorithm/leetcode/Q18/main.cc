feed3e055cc13372128023924f73d42024e428d9
mansterteddy <mansterteddy@gmail.com>
2022-07-04 23:09:32 +0800

    Fix Q18 for special cases.

--- algorithm/leetcode/Q18/main.cc
+++ algorithm/leetcode/Q18/main.cc
@@ -14,34 +14,48 @@ class Solution {
             }
 
             sort(nums.begin(), nums.end());
+            if(nums[0] > target && target >= 0) return res;
+            if(nums[nums.size()-1] < target && target <= 0) return res;
 
             int length = nums.size();
 
             for(int i = 0; i < length - 3; i++){
+
                 if(i > 0 && nums[i] == nums[i-1])
                     continue;
                 
-                if(nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target)
+                if( (long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3] == target){
+                    res.push_back({nums[i], nums[i+1], nums[i+2], nums[i+3]});
+                    break;
+                }
+
+                if( (long) nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target)
                     break;
 
-                if(nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target)
+                if( (long) nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target)
                     continue;
             
                 for(int j = i + 1; j < length - 2; ++j){
+
                     if(j > i + 1 && nums[j] == nums[j-1])
                         continue;
 
-                    if(nums[i] + nums[j] + nums[j+1] + nums[j+2] > target)
+                    if( (long) nums[i] + nums[j] + nums[j+1] + nums[j+2] == target){
+                        res.push_back({nums[i], nums[j], nums[j+1], nums[j+2]});
+                        break;
+                    }
+
+                    if( (long) nums[i] + nums[j] + nums[j+1] + nums[j+2] > target)
                         break;
 
-                    if(nums[i] + nums[j] + nums[length-2] + nums[length-1] < target)
+                    if( (long) nums[i] + nums[j] + nums[length-2] + nums[length-1] < target)
                         continue;
 
                     int left = j + 1;
                     int right = length - 1;
 
                     while(left < right){
-                        int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        long sum = nums[i] + nums[j] + nums[left] + nums[right];
                         if(sum == target){
                             res.push_back({nums[i], nums[j], nums[left], nums[right]});
                             while(left < right && nums[left] == nums[left+1])
@@ -59,8 +73,6 @@ class Solution {
                         }
                     }
                 }
-            
-            
             }
 
             return res;
